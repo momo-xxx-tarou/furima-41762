1cb43b182624cabca1ba9fc04ca601b56d7a630d
momo-xxx-tarou <momo.xxx.xyz@gmail.com>
2024-12-07 18:37:35 +0900

    環境変数

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,7 +10,7 @@ class OrdersController < ApplicationController
   def create
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
-      pay_item # 後ほど実装する支払い処理メソッド
+      pay_item
       @order_address.save
       redirect_to root_path
     else
@@ -48,6 +48,11 @@ class OrdersController < ApplicationController
   end
 
   def pay_item
-    # 後ほど実装する支払い処理
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: params[:token],
+      currency: 'jpy'
+    )
   end
 end
