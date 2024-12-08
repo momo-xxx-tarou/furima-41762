91bc2aa2c1a51e76de01e4eba8e7c20ff4a83973
momo-xxx-tarou <momo.xxx.xyz@gmail.com>
2024-12-08 19:52:17 +0900

    エラーの修正

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -3,9 +3,9 @@ FactoryBot.define do
     token { 'tok_abcdefghijk00000000000000000' }
     post_code { '123-4567' }
     prefecture_id { 1 }
-    city { '横浜市緑区' }
-    address { '青山1-1-1' }
-    building_name { '柳ビル103' }
+    city { '�京都' }
+    address { '1-1' }
+    building_name { '東京ハイツ' }
     phone_number { '09012345678' }
   end
 end
